4999a89952dbeec75d7b8de5639e06a860fc4127
Courtney Duggins <Marie1990@Courtneys-MacBook-Pro.local>
2018-10-04 22:28:19 -0400

    create initial folders

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Pizza Burger" , false);
+INSERT INTO parties (burger_name, devoured) VALUES ('Bacon Cheddar Smackdown', false);
+INSERT INTO parties (burger_name, devoured) VALUES ('Ghost Pepper Gouda Bacon Deluxe', false);
