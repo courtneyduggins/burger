768d180935e99fb751a7bf1f551d122d3e034515
Courtney Duggins <Marie1990@Courtneys-MacBook-Pro.local>
2018-10-06 15:20:21 -0400

    complete create, update, post and get routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,11 +1,66 @@
 var express = require("express");
-var burger = require("../models/burger.js");
-
 var router = express.Router();
 
+var burgers = require("../models/burger.js");
 
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res){
+    result.redirect("/burgers");
+  });
 
+router.get("/burgers", function(req, res) {
+    burgers.all(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+  
+  router.post("/api/burgers/create", function(req, res) {
+    burgers.create([
+      "burger_name", "devoured"
+    ], [
+      req.body.name, req.body.devoured
+    ], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put("/api/burgers/update/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burgers.update({
+      devoured: req.body.devoured
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
+  router.delete("/api/burgers/delete/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    cat.delete(condition, function(result) {
+      if (result.affectedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
 
+  
 
 
 
--- models/burger.js
+++ models/burger.js
@@ -1,11 +1,28 @@
 var orm = require("../config/orm.js");
 
-orm.selectAll("burger_name", "burgers");
-
-orm.insertOne("burgers","burger_name", "devoured");
-
-orm.updateOne("burgers", "burger_name", "devoured");
-
-
-
-module.exports = orm;
+var burger = {
+    all: function(cb) {
+      orm.all("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+      orm.create("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.update("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    },
+    delete: function(condition, cb) {
+      orm.delete("burgers", condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+  
+  // Export the database functions for the controller (catsController.js).
+  module.exports = burger;
