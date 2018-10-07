5f4dd0a916b6a6de18c7c757721c18a6bb712a42
Courtney Duggins <Marie1990@Courtneys-MacBook-Pro.local>
2018-10-07 18:05:55 -0400

    correct routes so new burger can be submitted

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,70 +3,47 @@ var router = express.Router();
 
 var burgers = require("../models/burger.js");
 
-// Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res){
-    burgers.all(function(data) {
-      var hbsObject = {
-        burgers: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
-    });
+router.get("/", function(req, res) {
+  burgers.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
   });
+});
 
-router.get("/burgers", function(req, res) {
-    burgers.all(function(data) {
-      var hbsObject = {
-        burgers: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
-    });
+router.post("/", function(req, res) {
+  burgers.create([
+    "burger_name", "devoured"
+  ], [
+    req.body.burger_name, req.body.devoured
+  ], function() {
+    res.redirect("/");
   });
-  
-  router.post("/burgers/create", function(req, res) {
-    burgers.create([
-      "burger_name", "devoured"
-    ], [
-      req.body.name, req.body.devoured
-    ], function(result) {
-      // Send back the ID of the new quote
-      res.json({ id: result.insertId });
-      res.render("index", hbsObject);
-    });
+});
+
+router.put("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burgers.update({
+    devoured: req.body.devoured
+  }, condition, function() {
+    res.redirect("/");
   });
-  
-  router.put("/burgers/update/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-  
-    console.log("condition", condition);
-  
-    burgers.update({
-      devoured: req.body.devoured
-    }, condition, function(result) {
-      if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
-      res.render("index", hbsObject);
-    });
+});
+
+router.delete("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burgers.delete(condition, function() {
+    res.redirect("/");
   });
-  
-  router.delete("/burgers/delete/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-  
-    cat.delete(condition, function(result) {
-      if (result.affectedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
-      res.render("index", hbsObject);
-    });
-  });
+});
+
+
   
 
   
